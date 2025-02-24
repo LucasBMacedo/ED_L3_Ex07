d39b27bfd7a8a330cafe2c65361365ee43a67dc1
lucasbezerrademacedo <lucasbezerrademacedo@192.168.0.17>
2025-02-24 18:08:37 -0300

    Segundo commit.

--- src/controller/FuncaoSoma.java
+++ src/controller/FuncaoSoma.java
@@ -7,8 +7,8 @@ public class FuncaoSoma {
 	}
 	public int FuncSoma(int n) {
 		if (n == 0) {
-			return 0;
+			return 0; // Parada para somar um número nulo.
 		}
-		return (n % 10) + FuncSoma(n/10);
+		return (n % 10) + FuncSoma(n/10); // Esta somando os restos com a chamado da funcao (Tive como base o exercicio 2).
 	}
 }
